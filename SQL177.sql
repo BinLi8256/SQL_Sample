da30b71869b000ba1d31cdc424b316501a9e1bbf
Bin Li <54602320+BinLi8256@users.noreply.github.com>
2025-08-03 13:01:17 -0500

    Update SQL177.sql

--- SQL177.sql
+++ SQL177.sql
@@ -1,10 +1,14 @@
 -- 177. Nth Highest Salary
 
------------ Question
--- Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+------------------------------------------ Question  ------------------------------------------
+-- Write a solution to find the nth highest distinct salary from the Employee table. 
+-- If there are less than n distinct salaries, return null.
 
------------ Intuition
--- This task requires to find the nth highest salary from the employee table. If there is no nth highest salary, the query should return null. This implies that we have to order the salary column in descending order and pick the nth entry. 
+
+------------------------------------------ Intuition ------------------------------------------ 
+-- This task requires to find the nth highest salary from the employee table. 
+-- If there is no nth highest salary, the query should return null. 
+-- This implies that we have to order the salary column in descending order and pick the nth entry. 
 
 --Note that in SQL, the order of execution for the clauses in a query is generally as follows:
 -- (1) FROM clause: this specifies the tables from which data will be retrieved
@@ -15,7 +19,7 @@
 -- (6) ORDER BY clause: this sorts the result set based on a specified column or expression
 -- (7) LIMIT/OFFSET clause: this limits the number of rows returned in the result set
 
------------ Solution
+------------------------------------------ Solution ------------------------------------------ 
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
 DECLARE M INT;
@@ -28,7 +32,7 @@ RETURN (
 );
 END
 
---------- BL Notes
+------------------------------------------ BL Notes ------------------------------------------ 
 CREATE FUNCTION function_name (para1 INT, para2 INT)
 RETURNS INT
 AS
